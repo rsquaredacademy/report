aacbdc68bbc863df8562e5e7edc7037e3f57b88f
rsquaredin <rsquaredin@gmail.com>
2019-09-27 22:15:54 +0530

    format code

--- R/prompt.R
+++ R/prompt.R
@@ -1,12 +1,12 @@
 #' @importFrom rstudioapi showPrompt
 ask_folder_name <- function() {
-  showPrompt(title = "Folder Title",
-                           message = "Title of the Folder",
-                           default = "report")
+  showPrompt(title   = "Folder Title",
+             message = "Title of the Folder",
+             default = "report")
 }
 
 ask_file_name <- function() {
-  showPrompt(title = "File Title",
+  showPrompt(  title = "File Title",
              message = "Title of the File",
              default = "report")
 }
@@ -19,38 +19,38 @@ add_ext <- function(folder_name, file_name) {
 #' @importFrom fs file_copy file_exists
 #' @importFrom here here
 copy_template <- function(folder_name, template_name) {
-  pkg_loc <- find.package("report")
+  pkg_loc       <- find.package("report")
   template_path <- glue(pkg_loc, '/templates/', template_name, '.Rmd')
-  report_path <- glue(here(), "/", folder_name, "/", template_name, ".Rmd")
+  report_path   <- glue(here(), "/", folder_name, "/", template_name, ".Rmd")
   if (!file_exists(report_path)) {
     file_copy(template_path, report_path)
   }
 }
 
 copy_yaml <- function(yaml_name) {
-  pkg_loc <- find.package("report")
+  pkg_loc       <- find.package("report")
   template_path <- glue(pkg_loc, '/yaml/_', yaml_name, '.yml')
-  report_path <- glue(here(), "/_", yaml_name, ".yml")
+  report_path   <- glue(here(), "/_", yaml_name, ".yml")
   if (!file_exists(report_path)) {
     file_copy(template_path, report_path)
   }
 }
 
 ask_title <- function() {
-  showPrompt(title = "Report Title",
+  showPrompt(title   = "Report Title",
              message = "Title of the Report",
              default = "report")
 }
 
 ask_author <- function() {
-  showPrompt(title = "Report Author",
+  showPrompt(title   = "Report Author",
              message = "Author of the Report",
              default = NULL)
 }
 
 #' @importFrom lubridate today
 ask_date <- function() {
-  showPrompt(title = "Report Date",
+  showPrompt(title   = "Report Date",
              message = "Date of the Report",
              default = as.character(today()))
 }
@@ -62,7 +62,7 @@ ask_model <- function() {
 }
 
 ask_type <- function() {
-  showPrompt(title = "Document Type",
+  showPrompt(title   = "Document Type",
              message = "Specify the document type (pdf or html)",
              default = "html")
 }
@@ -72,33 +72,33 @@ add_yaml <- function(report_file) {
 }
 
 ask_data_name <- function() {
-  showPrompt(title = "Data Name",
+  showPrompt(title   = "Data Name",
              message = "Specify the name of the data set",
              default = NULL)
 }
 
 ask_analysis_date <- function() {
-  showPrompt(title = "Analysis Date",
+  showPrompt(title   = "Analysis Date",
              message = "Specify the date of analysis",
              default = as.character(today()))
 }
 
 ask_customer_id <- function() {
-  showPrompt(title = "Customer ID",
+  showPrompt(title   = "Customer ID",
              message = "Specify the name of the column to identify customer id",
-             default = "customer_id")  
+             default = "customer_id")
 }
 
 ask_order_date <- function() {
-  showPrompt(title = "Order Date",
+  showPrompt(title   = "Order Date",
              message = "Specify the name of the column to identify order date",
-             default = "order_date")  
+             default = "order_date")
 }
 
 ask_revenue <- function() {
-  showPrompt(title = "Revenue",
+  showPrompt(title   = "Revenue",
              message = "Specify the name of the column to identify revenue",
-             default = "revenue")  
+             default = "revenue")
 }
 
 
--- R/report-blr.R
+++ R/report-blr.R
@@ -1,13 +1,13 @@
 #' Logistic regression report
 #'
-#' Generate detailed logistic regression report. 
+#' Generate detailed logistic regression report.
 #'
 #' @param use_yaml Use YAML file to specify report details.
 #'
 #' @examples
 #' \dontrun{
 #' model_data <- blorr::bank_marketing
-#' 
+#'
 #' # interactive
 #' report_blr()
 #'
@@ -23,19 +23,37 @@ report_blr <- function(use_yaml = FALSE) {
 
   if (use_yaml) {
     copy_yaml("logistic")
+
     file.edit(glue(here(), "/_logistic.yml"))
-    cat(glue('Please update the report details in the ', green('_logistic.yml'), ' file.'), "\n\n")
-    sleep_time <- showPrompt("Update Time", "Specify the time required in seconds to update the YAML file?", 30)
+
+    cat(
+      glue('Please update the report details in the ',
+           green('_logistic.yml'),
+           ' file.'
+      ), "\n\n")
+
+    sleep_time <- showPrompt(
+      title   = "Update Time",
+      message = "Specify the time required in seconds to update the YAML file?",
+      default = 30)
+
     Sys.sleep(sleep_time)
-    update_status <- showQuestion("File Status", "Have you updated the yaml file?", 
-                         "Yes", "No")
+
+    update_status <- showQuestion(
+      title   = "File Status",
+      message = "Have you updated the yaml file?",
+      ok      = "Yes",
+      cancel  ="No")
+
     documentSaveAll()
+
     if (update_status) {
-      logistic_yaml <- read_yaml(glue(here(), "/_logistic.yml"))  
+      logistic_yaml <- read_yaml(glue(here(), "/_logistic.yml"))
     } else {
-      stop("Please update the YAML file with the report details.", call. = FALSE)
+      stop("Please update the YAML file with the report details.",
+           call. = FALSE)
     }
-    
+
   }
 
   if (use_yaml) {
@@ -57,39 +75,69 @@ report_blr <- function(use_yaml = FALSE) {
     report_date   <- ask_date()
     data_name     <- ask_data_name()
     model_formula <- ask_model()
-    document_type <- ask_type() 
+    document_type <- ask_type()
     } else {
       stop("RStudio must be installed.", call. = FALSE)
     }
-    
+
   }
 
   # prep report folders and file
-  cat(blue$bold(symbol$tick), glue('Creating ', green("'"), green(folder_name), 
-                                 green("'"), ' folder'), '\n')
+  cat(blue$bold(symbol$tick),
+      glue('Creating ',
+           green("'"),
+           green(folder_name),
+           green("'"),
+           ' folder'
+      ), '\n')
+
   dir_create(folder_name)
+
   if (use_yaml) {
     Sys.sleep(2)
-    cat(blue$bold(symbol$tick), glue('Moving ', green("'_logistic.yml'"), ' file to ', folder_name), '\n')
-    file_move(glue(here(), "/_logistic.yml"), glue(here(), "/", folder_name, "/_logistic.yml"))
+
+    cat(blue$bold(symbol$tick),
+        glue('Moving ',
+             green("'_logistic.yml'"),
+             ' file to ',
+             folder_name
+        ), '\n')
+
+    file_move(glue(here(), "/_logistic.yml"),
+              glue(here(), "/", folder_name, "/_logistic.yml"))
   }
 
   # add .Rmd extension to file_name
   report_file <- add_ext(folder_name, file_name)
+
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Creating ', green("'"), green(file_name), 
-                                 green("'"), ' file'), '\n')
+
+  cat(blue$bold(symbol$tick),
+      glue('Creating ',
+           green("'"),
+           green(file_name),
+           green("'"),
+           ' file'
+      ), '\n')
+
   file_create(report_file)
 
   # copy template from inst folder
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Copying report template into ', green("'"), green(folder_name), 
-                                 green("'"), ' folder'), '\n')
+
+  cat(blue$bold(symbol$tick),
+      glue('Copying report template into ',
+           green("'"),
+           green(folder_name),
+           green("'"),
+           ' folder'
+      ), '\n')
+
   copy_template(folder_name, "blorr_template")
 
   # add yaml
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Adding YAML to ', green("'"), green(file_name), 
+  cat(blue$bold(symbol$tick), glue('Adding YAML to ', green("'"), green(file_name),
                                  green("'"), ' file'), '\n')
   cat('---\n', file = report_file, append =FALSE)
   cat(glue('title: "', {report_title}, '"', '\n\n'), file = report_file,
@@ -121,19 +169,40 @@ report_blr <- function(use_yaml = FALSE) {
 
   # load data
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Loading data ', green("'"), green(data_name), 
-                                 green("'")), '\n')
+
+  cat(blue$bold(symbol$tick),
+      glue('Loading data ',
+           green("'"),
+           green(data_name),
+           green("'")
+      ), '\n')
+
   prep_data(data_name)
 
   # build and view report
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Generating ', green("'"), green(report_title), 
-                                 green("'")), '\n')
+
+  cat(blue$bold(symbol$tick),
+      glue('Generating ',
+           green("'"),
+           green(report_title),
+           green("'")
+      ), '\n')
+
   render(report_file, quiet = TRUE)
+
   report_name <- glue(folder_name, "/", file_name, ".", document_type)
+
   Sys.sleep(2)
-  cat(blue$bold(symbol$tick), glue('Opening ', green("'"), green(report_title), 
-                                 green("'"), ' in default browser'), '\n')
+
+  cat(blue$bold(symbol$tick),
+      glue('Opening ',
+           green("'"),
+           green(report_title),
+           green("'"),
+           ' in default browser'
+      ), '\n')
+
   browseURL(report_name)
 
 }
